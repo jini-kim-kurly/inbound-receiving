1679b522a5a81977271e85a2e8b26c9685fd4fb8
jini <jini.kim@kurlycorp.com>
2023-04-12 11:17:55 +0900

    test

---
+++ src/main/java/com/kurly/inbound/HelloInboundReceiving2.java
@@ -0,0 +1,15 @@
+package com.kurly.inbound;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class HelloInboundReceiving {
+
+    public String getMessageFromExternal(){
+        return "";
+    }
+
+}
+
